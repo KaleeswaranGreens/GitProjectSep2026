427914589b446ecc6aee16d6cd32febc3277e72d
KaleeswaranGreens <kaleswaranomrbranch@gmail.com>
2026-09-12 14:38:41 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,4 +2,7 @@ public class Employee{
   public void empName(){
     System.out.println("Employee Name is : Karan");
   }
+  public void empId(){
+    System.out.println("Employee Id is : 209");
+  }
 }
