cc285b9efdbb30f480492b449847370f60a9a930
KaleeswaranGreens <kaleswaranomrbranch@gmail.com>
2026-09-12 15:06:29 +0530

    Updated and Created

--- Sample.java
+++ Sample.java
@@ -3,14 +3,19 @@ public class Employee{
   public void empName(){
     System.out.println("Employee Name is : Karan");
   }
-  
+
   public void empId(){
     System.out.println("Employee Id is : 210");
+  }
+
+  public void empAddress(){
+    System.out.println("Employee Address is : Chennai");
   }
 
   public static void main(String[] args) {
     Employee e = new Employee();
     e.empId();
     e.empName();
+    e.empAddress();
   }
 }
--- Sample2.java
+++ Sample2.java
@@ -1,8 +1,9 @@
 
 public class Sample2 {
 
+
     public static void main(String[] args) {
-        System.out.println("Sample2 File Created..");
+        System.out.println("Sample2.java is updated...");
     }
     
 }
